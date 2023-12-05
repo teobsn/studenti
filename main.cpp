4d88db62eda1986fcb3befebc89ffbea099eb1d2
teobsn <teobsn@gmail.com>
2023-12-05 08:52:56 +0200

    modificat baza de date (inversat nume si prenume)

--- main.cpp
+++ main.cpp
@@ -16,6 +16,20 @@
 #include "database.h"
 #include "search.h"
 
+void database_sort_az()
+{
+    for (int i = 1; i <= database_size - 1; i++)
+        for (int j = i + 1; j <= database_size; j++)
+        {
+            if (strcmp(studenti[i].nume, studenti[j].nume) > 0 || (strcmp(studenti[i].nume, studenti[j].nume) == 0 && strcmp(studenti[i].prenume, studenti[j].prenume) > 0))
+            {
+                struct student aux = studenti[i];
+                studenti[i] = studenti[j];
+                studenti[j] = aux;
+            }
+        }
+}
+
 bool database_ver_corigent(int i)
 {
     return (studenti[i].medie >= medie_min);
@@ -36,10 +50,18 @@ int main()
     database_read();
 
     // Ncurses init
+    /*
     initscr();
     raw();
     noecho();
     keypad(stdscr, TRUE);
+    */
+    for (int i = 1; i <= database_length; i++)
+        std::cerr << studenti[i].cod << " " << studenti[i].nume << " " << studenti[i].prenume << " " << studenti[i].dn.an << "/" << studenti[i].dn.luna << "/" << studenti[i].dn.zi << " " << studenti[i].grupa << " " << studenti[i].medie << " " << studenti[i].val_bursa << "\n";
+
+    database_sort_az();
+    for (int i = 1; i <= database_length; i++)
+        std::cerr << studenti[i].cod << " " << studenti[i].nume << " " << studenti[i].prenume << " " << studenti[i].dn.an << "/" << studenti[i].dn.luna << "/" << studenti[i].dn.zi << " " << studenti[i].grupa << " " << studenti[i].medie << " " << studenti[i].val_bursa << "\n";
 
     return 0;
 }
